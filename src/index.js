bcabf4d4e426d785afa74895df9c3016e07ac141
jpmunich <parkercoyle8@gmail.com>
2023-07-30 10:38:23 -0400

    Calls only the root div

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import './style.css';
-import Forest from './forest.jpg';
 
-const p = document.createElement('h1');
-p.innerText = "Whereas";
+const content = document.getElementById('content');
 
-const forest = document.createElement('img');
-forest.src = Forest;
-
-document.body.appendChild(p);
-document.body.appendChild(forest);
