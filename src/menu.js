25ff8bf79008d66cbbc0f6cb73afd494c110ba91
jpmunich <parkercoyle8@gmail.com>
2023-07-31 11:37:00 -0400

    Organized code and made function for menu skeleton

--- src/menu.js
+++ src/menu.js
@@ -1,29 +1,13 @@
-import { createHomePage, createLink } from "./home";
-export default function createMenu() {
-    const title = document.createElement('h1');
-    title.innerText = 'Greeks Gyros';
-    title.classList.add('menu-title');
+import { openNewTab } from "./element-creator";
 
-    const menuOverlay = document.createElement('div');
-    menuOverlay.classList.add('menu-overlay');
-
-    const linkContainer = document.createElement('div')
-    linkContainer.classList.add('link-container');
-
-    document.getElementById('content').appendChild(menuOverlay);
-    menuOverlay.appendChild(title);
-    menuOverlay.appendChild(linkContainer);
-
-    createLink(linkContainer, 'Home', 'link', 'home');
-    createLink(linkContainer, 'Menu', 'link', 'menu');
-    createLink(linkContainer, 'Contact', 'link', 'contact');
-
+function createMenu() {
     document.querySelector('[data-button-type="home"]').addEventListener('click', () => {
-        destroyMenuPage();
-        createHomePage();
+        openNewTab('.menu-overlay', 'home');
+    })
+
+    document.querySelector('[data-button-type="contact"]').addEventListener('click', () => {
+        openNewTab('.menu-overlay', 'contact');
     })
 }
 
-function destroyMenuPage() {
-    document.querySelector('.menu-overlay').remove();
-}
+export { createMenu };
