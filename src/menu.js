18ea743fc434a35089f1d2e292f0b9d30c1f2aee
jpmunich <parkercoyle8@gmail.com>
2023-08-01 19:05:35 -0400

    10 menu items show up on screen

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,6 @@
-import { openNewTab } from "./element-creator";
+import { openNewTab, createTextElement } from "./element-creator";
 
-function createMenu() {
-    document.getElementById('content').style.backgroundImage = 'url(./background-img.jpg)';
-
+function allowMenuSwitches() {
     document.querySelector('[data-button-type="home"]').addEventListener('click', () => {
         openNewTab('.menu-overlay', 'home');
     })
@@ -10,6 +8,27 @@ function createMenu() {
     document.querySelector('[data-button-type="contact"]').addEventListener('click', () => {
         openNewTab('.menu-overlay', 'contact');
     })
+}
+
+function createMenu() {
+    document.getElementById('content').style.backgroundImage = 'url(./background-img.jpg)';
+
+    const menuPageContainer = document.createElement('div');
+    menuPageContainer.classList.add('menu-page-container');
+    document.getElementById('content').appendChild(menuPageContainer);
+
+    createTextElement('h2', 'Menu', menuPageContainer);
+
+    const menuItemContainer = document.createElement('div');
+    menuItemContainer.classList.add('menu-item-container');
+    menuPageContainer.appendChild(menuItemContainer);
+
+    for (let i = 0; i < 10; i++) {
+        const menuItem = document.createElement('div');
+        menuItem.classList.add('menu-item');
+        menuItem.innerText = 'Hello';
+        menuItemContainer.appendChild(menuItem);
+    }
 }
 
 export { createMenu };
