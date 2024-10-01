aa8bd1c685a379a3168aea1c7d5b63016b4a073a
jpmunich <parkercoyle8@gmail.com>
2024-09-30 20:56:56 -0400

    Fix broken link

--- src/home.js
+++ src/home.js
@@ -1,30 +1,35 @@
-import { createLink, openNewTab, } from "./element-creator";
+import { createLink, openNewTab } from "./element-creator";
 
 function createHomePage() {
-    document.getElementById('content').style.backgroundImage = 'url(../dist/images/background-img.jpg)';
+  document.getElementById("content").style.backgroundImage =
+    "url(./images/background-img.jpg)";
 
-    const titleOverlay = document.createElement('div');
-    const title = document.createElement('h1');
-    const linkContainer = document.createElement('div');
+  const titleOverlay = document.createElement("div");
+  const title = document.createElement("h1");
+  const linkContainer = document.createElement("div");
 
-    linkContainer.classList.add('link-container');
-    titleOverlay.classList.add('title-overlay');
-    title.innerText = 'Greeks Gyros and More!';
+  linkContainer.classList.add("link-container");
+  titleOverlay.classList.add("title-overlay");
+  title.innerText = "Greeks Gyros and More!";
 
-    document.getElementById('content').appendChild(titleOverlay);
-    titleOverlay.appendChild(title);
-    titleOverlay.appendChild(linkContainer);
-    createLink(linkContainer, 'Home', 'link', 'home');
-    createLink(linkContainer, 'Menu', 'link', 'menu');
-    createLink(linkContainer, 'Contact', 'link', 'contact');
+  document.getElementById("content").appendChild(titleOverlay);
+  titleOverlay.appendChild(title);
+  titleOverlay.appendChild(linkContainer);
+  createLink(linkContainer, "Home", "link", "home");
+  createLink(linkContainer, "Menu", "link", "menu");
+  createLink(linkContainer, "Contact", "link", "contact");
 
-    document.querySelector('[data-button-type="menu"]').addEventListener('click', () => {
-        openNewTab('.title-overlay', 'menu');
-    })
-    
-    document.querySelector('[data-button-type="contact"]').addEventListener('click', () => {
-        openNewTab('.title-overlay', 'contact');
-    })
+  document
+    .querySelector('[data-button-type="menu"]')
+    .addEventListener("click", () => {
+      openNewTab(".title-overlay", "menu");
+    });
+
+  document
+    .querySelector('[data-button-type="contact"]')
+    .addEventListener("click", () => {
+      openNewTab(".title-overlay", "contact");
+    });
 }
 
 export { createHomePage };
